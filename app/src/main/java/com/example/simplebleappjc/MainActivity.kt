4a4bb4d7787bf04e06224c0e972fbad6726c1d18
kmeyerhshl <118357042+kmeyerhshl@users.noreply.github.com>
2024-06-25 17:18:48 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/simplebleappjc/MainActivity.kt
+++ app/src/main/java/com/example/simplebleappjc/MainActivity.kt
@@ -62,7 +62,8 @@ class MainActivity : ComponentActivity() {
         val permissions = listOf(
             android.Manifest.permission.ACCESS_FINE_LOCATION,
             android.Manifest.permission.ACCESS_COARSE_LOCATION,
-            android.Manifest.permission.BLUETOOTH_CONNECT
+            android.Manifest.permission.BLUETOOTH_CONNECT,
+            android.Manifest.permission.BLUETOOTH_SCAN
         )
 
         val missingPermissions = permissions.filter { permission ->
